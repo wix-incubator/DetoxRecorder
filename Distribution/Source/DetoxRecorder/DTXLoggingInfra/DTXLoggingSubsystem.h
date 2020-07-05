6743792b5fb851a811cd44230e60268256a18a45
Leo Natan <leo.natan@outlook.com>
2020-07-06 00:38:03 +0300

    1.0.116

---
+++ Distribution/Source/DetoxRecorder/DTXLoggingInfra/DTXLoggingSubsystem.h
@@ -0,0 +1,9 @@
+//
+//  DTXLoggingSubsystem.h
+//  dtxlog
+//
+//  Created by Leo Natan (Wix) on 5/3/20.
+//  Copyright © 2020 Leo Natan. All rights reserved.
+//
+
+#define DTX_LOG_SUBSYSTEM "Test"
