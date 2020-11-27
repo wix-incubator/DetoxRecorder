861ac946b85950ea639f32e67065110bce938e43
Leo Natan <leo.natan@outlook.com>
2020-11-27 23:17:44 +0200

    Update build process to support Apple Silicon

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ rm -rf "${SCRIPTPATH}/Distribution/Source"
 
 echo -e "\033[1;34mBuilding DetoxRecorder.framework\033[0m"
 
-xcodebuild -project "${SCRIPTPATH}/DetoxRecorder/DetoxRecorder.xcodeproj" build -configuration Release -scheme DetoxRecorderFramework -derivedDataPath "${SCRIPTPATH}/Build" -quiet
-cp -R "${SCRIPTPATH}/Build/Build/Products/Release-universal/DetoxRecorder.framework" "${SCRIPTPATH}/Distribution"
+xcodebuild -project "${SCRIPTPATH}/DetoxRecorder/DetoxRecorder.xcodeproj" build -configuration Release -scheme DetoxRecorder -destination 'generic/platform=iOS Simulator' -derivedDataPath "${SCRIPTPATH}/Build" -quiet
+cp -R "${SCRIPTPATH}/Build/Build/Products/Release-iphonesimulator/DetoxRecorder.framework" "${SCRIPTPATH}/Distribution"
 
 echo -e "\033[1;34mBuilding Detox Recorder CLI\033[0m"
 
